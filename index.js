b058ef3e000a088aa13d0b980a3214399e6ed217
Lashawn Butler <lashawnbutler50@gmail.com>
2024-04-23 17:10:11 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(drivers, query) {
+    const lowerQuery = query.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase().includes(lowerQuery))
+    return matchingDrivers
+}
+function fuzzyMatch(drivers, prefix) {
+    const lowerPrefix = prefix.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase().startsWith(lowerPrefix)
+    );
+    
+    return matchingDrivers;
+  }
+  function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => driver.name ===name);
+    return matchingDrivers
+  }
